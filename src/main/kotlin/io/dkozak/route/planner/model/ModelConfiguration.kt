40c91b3b49f47d4fa47b9f699cefca0afd9cddcf
dkozak <dkozak94@gmail.com>
2019-06-04 11:26:16 +0100

    update default price per km to 0.2 euro

--- src/main/kotlin/io/dkozak/route/planner/model/ModelConfiguration.kt
+++ src/main/kotlin/io/dkozak/route/planner/model/ModelConfiguration.kt
@@ -6,7 +6,7 @@ data class ModelConfiguration(
         val wantedResourceUnits: Int,
         val suppliers: PVector<Supplier>,
         val dcPos: Location = Pair(500, 500),
-        val costPerKm: MoneyPerKm = MoneyPerKm(0.3),
+        val costPerKm: MoneyPerKm = MoneyPerKm(0.2),
         val truckCapacity: Int = 200,
         val costTruckPerDay: Money = Money(50.0)
 )
--- src/main/resources/config.kts
+++ src/main/resources/config.kts
@@ -5,7 +5,7 @@ model {
     wantedResourceUnits = 500
     suppliersFile = "./src/main/resources/routes.csv"
     dcPos = 500 to 500
-    costPerKm = 0.3
+    costPerKm = 0.2
     truckCapacity = 200
     costTruckPerDay = 50.0
 }
